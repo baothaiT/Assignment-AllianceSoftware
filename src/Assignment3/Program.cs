3483a0e1e7da430337ff4fce723d15e201093800
BAO\thaib <thaibao0225@gmail.com>
2025-01-13 07:18:28 +0700

    Create - Assignment 3

--- src/Assignment3/Program.cs
+++ src/Assignment3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static bool IsMeowString(string X)
+    {
+        if (string.IsNullOrEmpty(X)) return false;
+
+        // Biến để theo dõi trạng thái hiện tại (m, e, o, w)
+        char currentChar = 'm';
+
+        foreach (char ch in X)
+        {
+            if (ch != 'm' && ch != 'e' && ch != 'o' && ch != 'w')
+                return false;
+
+            if (ch < currentChar)
+                return false;
+
+            currentChar = ch;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        // Các test cases
+        string[] testStrings = { "mmmmeoooow", "meow", "meowwww", "mmeeooww", "meo", "mow", "mew", "maaow" };
+
+        foreach (var str in testStrings)
+        {
+            Console.WriteLine($"{str}: {IsMeowString(str)}");
+        }
+    }
+}
