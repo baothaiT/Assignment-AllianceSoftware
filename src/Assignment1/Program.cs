050c9bf7ab14daa341041419ade275cdfc3567c2
BAO\thaib <thaibao0225@gmail.com>
2025-01-13 07:20:33 +0700

    Rollback - Assignment

--- src/Assignment1/Program.cs
+++ src/Assignment1/Program.cs
@@ -1,58 +1,47 @@
-﻿using AssignmentAllianceitsc.Serivces;
-using System.Buffers;
+﻿using AssignmentAllianceitsc.Serivces.Assignment1;
 using System.Diagnostics;
-using System.Drawing;
-using System.Numerics;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        UseCheckNumInArray();
-    }
+        int[] array = Enumerable.Range(1, 2147483591).ToArray();
+        int searchValue = 20000;
 
-    public static void UseCheckNumInArray()
-    {
-        int[] array = Enumerable.Range(1, 1000000).ToArray();
-        Stopwatch overallStopwatch = new Stopwatch();
-        overallStopwatch.Start();
-        int total = 0;
-        object consoleLock = new object();
-
-        Parallel.For(0, 1000000, i =>
+        int[] testSizes = { 0, 100000, 150000, 200000, 300000, 500000, 1000000, 10000000, 2147483591 };
+        foreach (int size in testSizes)
         {
-            MeasureSearchPerformance(array, array.Length, i, consoleLock);
-            Interlocked.Increment(ref total);
-        });
+            Console.WriteLine($"Running CheckNumInArray for array size: {size}");
 
-        overallStopwatch.Stop();
-        Console.WriteLine($"Total ran: {total}");
-        Console.WriteLine($"Total Time: {overallStopwatch.Elapsed.TotalMilliseconds:F2} ms");
+            MeasureSearchPerformance(array.Take(size).ToArray(), size, searchValue);
+        }
     }
-
-    public static void MeasureSearchPerformance(int[] array, int size, int searchValue, object consoleLock)
+    public static void MeasureSearchPerformance(int[] array, int size, int searchValue)
     {
-        Stopwatch stopwatch = Stopwatch.StartNew();
-        bool result = false;
+        Stopwatch stopwatch = new Stopwatch();
 
         if (size <= 300000)
         {
             var binaryProcessor = new ArrayProcessor(new BinarySearchStrategy());
             var arrayBinary = binaryProcessor.InitializeArray(array);
 
-            result = binaryProcessor.CheckNumInArray(arrayBinary, arrayBinary.Length, searchValue);
+            stopwatch.Start();
+            bool resultBinary = binaryProcessor.CheckNumInArray(arrayBinary, arrayBinary.Length, searchValue);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Binary Search: {searchValue} found? {resultBinary}. Time: {stopwatch.Elapsed.TotalMilliseconds:F2} ms");
         }
         else
         {
             var parallelProcessor = new ArrayProcessor(new ParallelSearchStrategy());
-            parallelProcessor.CheckNumInArray(array, Math.Min(size, 10), searchValue); // Warm-up
-            result = parallelProcessor.CheckNumInArray(array, array.Length, searchValue);
-        }
-        stopwatch.Stop();
+            parallelProcessor.CheckNumInArray(array, Math.Min(size, 10), searchValue); // Warm-up to reduce overhead
 
-        lock (consoleLock)
-        {
-            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId}, SearchValue: {searchValue}, Found: {result}, Time: {stopwatch.Elapsed.TotalMilliseconds:F2} ms");
+            stopwatch.Start();
+            bool resultParallel = parallelProcessor.CheckNumInArray(array, array.Length, searchValue);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Parallel Linear Search: {searchValue} found? {resultParallel}. Time: {stopwatch.Elapsed.TotalMilliseconds:F2} ms");
         }
+        Console.WriteLine();
     }
 }
